6342d2ab9d4747af2de64d7622bfbdf112682c03
ThomasN0712 <92189845+ThomasN0712@users.noreply.github.com>
2023-10-28 16:24:53 -0700

    Update files

--- RoadController.java
+++ RoadController.java
@@ -6,32 +6,103 @@
  */
 
 import java.util.ArrayList;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.Condition;
+import java.util.Random;
 
 public class RoadController {
    // Create a queue of threads
-   static ArrayList<Thread> thread_queue = new ArrayList<Thread>();
+   static ArrayList<Thread> thread_queue = new ArrayList<>();
+
+   static ArrayList<String> villager_identity = new ArrayList<>();
+
+   static ReentrantLock road = new ReentrantLock();
+
+   static Condition condition = road.newCondition();
+
+   public static void enter_identity(String identity) {
+      villager_identity.add(identity);
+   }
 
    public static void enter_queue(Thread T) {
       thread_queue.add(T);
    }
 
-   public static void main(String[] args) {
-      // Initilize east and west villagers
-      for (int i = 1; i <= 3; i++) {
-         Thread east_villager = new Thread(new East_village(i));
-         Thread west_villager = new Thread(new West_village(i));
-         east_villager.start();
-         west_villager.start();
+   public static void enter_road(Thread T, String identity) {
+      String[] actions = { " is watching the sunset for ", " is doing 326 Project 3 for ",
+            " is Netflix and waiting for ", " is happy happy happy (Jumping cat meme) for " };
+      int action = new Random().nextInt(actions.length);
+      // if road is not locked, open to use
+      Random rand = new Random();
+      // walks on the road for like 3 seconds
+      int randomNum = rand.nextInt(3) + 1;
+      try {
+         System.out.println(identity + actions[action] + randomNum + " seconds");
+         Thread.sleep(randomNum * 1000);
+      } catch (InterruptedException e) {
+         e.printStackTrace();
+      }
+      System.out.println(identity + " is done with the road. They are leaving the road now.");
+      road.unlock();
+   }
+
+   public static void test(Thread T) {
+      // if road is not locked, open to use
+      if (road.isLocked() == false) {
+         road.lock();
+         System.out.println(villager_identity.get(0) + " is traveling on the road.");
+         enter_road(thread_queue.get(0), villager_identity.get(0));
+         thread_queue.remove(0);
+         villager_identity.remove(0);
+      }
+      if (road.isLocked() == true) {
          try {
-            east_villager.join();
-            west_villager.join();
+            condition.await();
          } catch (InterruptedException e) {
             e.printStackTrace();
          }
+         System.out.println("Road is locked. " + villager_identity.get(0) + " is waiting.");
+         test(T);
       }
-      // Wait for all threads to finish
-      System.out.println(thread_queue);
-
    }
 
+   public static void main(String[] args) {
+      // Create threads
+      Thread east_village_1 = new Thread(new East_village(1));
+      Thread west_village_1 = new Thread(new West_village(1));
+      Thread east_village_2 = new Thread(new East_village(2));
+      Thread west_village_2 = new Thread(new West_village(2));
+      Thread east_village_3 = new Thread(new East_village(3));
+      Thread west_village_3 = new Thread(new West_village(3));
+      Thread east_village_4 = new Thread(new East_village(4));
+      Thread west_village_4 = new Thread(new West_village(4));
+
+      // Start threads
+      east_village_1.start();
+      west_village_1.start();
+      east_village_2.start();
+      west_village_2.start();
+      east_village_3.start();
+      west_village_3.start();
+      east_village_4.start();
+      west_village_4.start();
+
+      // Wait for threads to finish
+      try {
+         east_village_1.join();
+         west_village_1.join();
+         east_village_2.join();
+         west_village_2.join();
+         east_village_3.join();
+         west_village_3.join();
+         east_village_4.join();
+         west_village_4.join();
+      } catch (InterruptedException e) {
+         System.out.println("Interrupted");
+      }
+
+      while (thread_queue.size() > 0) {
+         test(thread_queue.get(0));
+      }
+   }
 }
--- West_village.java
+++ West_village.java
@@ -7,17 +7,23 @@ public class West_village implements Runnable {
       this.villager_number = villager_number;
    }
 
+   public String get_villager_identity() {
+      return "West villager " + this.villager_number;
+   }
+
    @Override
    public void run() {
-      // Sleep for random time from 1 to 5 seconds
+      // Sleep for random time from 1 to 3 seconds
       Random rand = new Random();
-      int randomNum = rand.nextInt(5) + 1;
+      int randomNum = rand.nextInt(30) + 1;
       try {
-         Thread.sleep(randomNum * 1000);
+         Thread.sleep(randomNum * 100);
       } catch (InterruptedException e) {
          e.printStackTrace();
       }
-      System.out.println("West villager " + this.villager_number + " is ready to cross the bridge");
+      System.out.println(this.get_villager_identity() + " wake up after " + randomNum * 100
+            + "ms nap. They are ready to cross the road now. Entering queue.");
       RoadController.enter_queue(Thread.currentThread());
+      RoadController.enter_identity(get_villager_identity());
    }
 }
